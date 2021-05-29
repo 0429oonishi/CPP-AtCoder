d7930c1de1e28454fcbeb2a69d1b720a4869704c
0429oonishi <oonishireon110@gmail.com>
2021-05-29 23:05:47 +0900

    add: ex5

---
+++ CPP-practice/ex5.cpp
@@ -0,0 +1,15 @@
+//
+//  ex5.cpp
+//  CPP-practice
+//
+//  Created by 大西玲音 on 2021/05/29.
+//
+
+#include "include/bits/stdc++.h"
+using namespace std;
+
+//int main() {
+//    int A, B;
+//    cin >> A >> B;
+//    cout << A + B << endl;
+//}

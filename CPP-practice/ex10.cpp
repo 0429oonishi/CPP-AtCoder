aefc763bdb38dab127b842ee9ab785ebeaac2be6
0429oonishi <oonishireon110@gmail.com>
2021-05-29 23:06:26 +0900

    add: ex10

---
+++ CPP-practice/ex10.cpp
@@ -0,0 +1,28 @@
+//
+//  ex10.cpp
+//  CPP-practice
+//
+//  Created by 大西玲音 on 2021/05/29.
+//
+
+#include "include/bits/stdc++.h"
+using namespace std;
+
+//int main() {
+//    int A, B;
+//    cin >> A >> B;
+//    int i = 0;
+//    int j = 0;
+//    cout << "A:";
+//    while (i < A) {
+//        cout << "]";
+//        i++;
+//    }
+//    cout << endl;
+//    cout << "B:";
+//    while (j < B) {
+//        cout << "]";
+//        j++;
+//    }
+//    cout << endl;
+//}

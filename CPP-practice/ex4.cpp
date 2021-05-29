b6c137a96e36a4294cb46904411262beb2bdfd9b
0429oonishi <oonishireon110@gmail.com>
2021-05-29 23:05:35 +0900

    add: ex4

---
+++ CPP-practice/ex4.cpp
@@ -0,0 +1,17 @@
+//
+//  ex4.cpp
+//  CPP-practice
+//
+//  Created by 大西玲音 on 2021/05/29.
+//
+
+#include "include/bits/stdc++.h"
+using namespace std;
+
+//int main() {
+//    int seconds = 365 * 24 * 60 * 60;
+//    cout << seconds << endl;
+//    cout << seconds * 2 << endl;
+//    cout << seconds * 5 << endl;
+//    cout << seconds * 10 << endl;
+//}

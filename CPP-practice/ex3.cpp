441736caba0ae9c655b3f8f69f35c0b8fa2e7d8e
0429oonishi <oonishireon110@gmail.com>
2021-05-29 23:05:28 +0900

    add: ex3

---
+++ CPP-practice/ex3.cpp
@@ -0,0 +1,13 @@
+//
+//  ex3.cpp
+//  CPP-practice
+//
+//  Created by 大西玲音 on 2021/05/29.
+//
+
+#include "include/bits/stdc++.h"
+using namespace std;
+
+//int main() {
+//    cout << 100 * (100 + 1) / 2 << endl;
+//}

e6f02e39cc87bc211f33bac1dd07936fa34a7c34
0429oonishi <oonishireon110@gmail.com>
2021-05-29 23:05:55 +0900

    add: ex6

---
+++ CPP-practice/ex6.cpp
@@ -0,0 +1,35 @@
+//
+//  ex6.cpp
+//  CPP-practice
+//
+//  Created by 大西玲音 on 2021/05/29.
+//
+
+#include "include/bits/stdc++.h"
+using namespace std;
+
+//int main() {
+//    int A, B;
+//    string op;
+//    cin >> A >> op >> B;
+//    if (op == "+") {
+//        cout << A + B << endl;
+//    }
+//    else if (op == "-") {
+//        cout << A - B << endl;
+//    }
+//    else if (op == "*") {
+//        cout << A * B << endl;
+//    }
+//    else if (op == "/") {
+//        if (B == 0) {
+//            cout << "error" << endl;
+//        }
+//        else {
+//            cout << A / B << endl;
+//        }
+//    }
+//    else {
+//        cout << "error" << endl;
+//    }
+//}

6e6314f97481bdb7eae484681cf410dcabc1c11c
0429oonishi <oonishireon110@gmail.com>
2021-05-29 23:06:17 +0900

    add: ex9

---
+++ CPP-practice/ex9.cpp
@@ -0,0 +1,22 @@
+//
+//  ex9.cpp
+//  CPP-practice
+//
+//  Created by 大西玲音 on 2021/05/29.
+//
+
+#include "include/bits/stdc++.h"
+using namespace std;
+
+//int main() {
+//    int x, a, b;
+//    cin >> x >> a >> b;
+//    x++;
+//    cout << x << endl;
+//    x *= (a + b);
+//    cout << x << endl;
+//    x *= x;
+//    cout << x << endl;
+//    x--;
+//    cout << x << endl;
+//}

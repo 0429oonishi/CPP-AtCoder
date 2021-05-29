0efcec03be810826d3b3738eb37d4c8c1d6399a7
0429oonishi <oonishireon110@gmail.com>
2021-05-29 23:05:09 +0900

    add: ex1

---
+++ CPP-practice/ex1.cpp
@@ -0,0 +1,14 @@
+//
+//  ex1.cpp
+//  CPP-practice
+//
+//  Created by 大西玲音 on 2021/05/29.
+//
+
+#include "include/bits/stdc++.h"
+using namespace std;
+
+//int main() {
+//    cout << "こんにちは" << endl;
+//    cout << "AtCoder" << endl;
+//}

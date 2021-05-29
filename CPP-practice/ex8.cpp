c452ba37752f36a8247bd5d99d3952e3f1b2cc4b
0429oonishi <oonishireon110@gmail.com>
2021-05-29 23:06:11 +0900

    add: ex8

---
+++ CPP-practice/ex8.cpp
@@ -0,0 +1,22 @@
+//
+//  ex8.cpp
+//  CPP-practice
+//
+//  Created by 大西玲音 on 2021/05/29.
+//
+
+#include "include/bits/stdc++.h"
+using namespace std;
+
+//int main() {
+//    int p;
+//    cin >> p;
+//    if (p == 2) {
+//        string text;
+//        cin >> text;
+//        cout << text << "!" << endl;
+//    }
+//    int price, N;
+//    cin >> price >> N;
+//    cout << price * N << endl;
+//}

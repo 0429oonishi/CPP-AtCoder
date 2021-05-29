4c61d416d54e358ddad8d83de2af56c8aeb7e05e
0429oonishi <oonishireon110@gmail.com>
2021-05-29 23:05:19 +0900

    add: ex2

---
+++ CPP-practice/ex2.cpp
@@ -0,0 +1,14 @@
+//
+//  ex2.cpp
+//  CPP-practice
+//
+//  Created by 大西玲音 on 2021/05/29.
+//
+
+#include "include/bits/stdc++.h"
+using namespace std;
+
+//int main() {
+//    cout << "いつも" << 2525 << endl;
+//    cout << "AtCoderくん" << endl;
+//}

06865316b7a0f96e85c6c5f38d7b9d7881e030b8
0429oonishi <oonishireon110@gmail.com>
2021-05-29 23:06:32 +0900

    add: ex11

---
+++ CPP-practice/ex11.cpp
@@ -0,0 +1,36 @@
+//
+//  ex11.cpp
+//  CPP-practice
+//
+//  Created by 大西玲音 on 2021/05/29.
+//
+
+#include "include/bits/stdc++.h"
+using namespace std;
+
+//int main() {
+//    int N, A;
+//    cin >> N >> A;
+//    for (int i = 0; i < N; i++) {
+//        int x;
+//        string op;
+//        cin >> op >> x;
+//        if (op == "+") {
+//            A += x;
+//        }
+//        else if (op == "-") {
+//            A -= x;
+//        }
+//        else if (op == "*") {
+//            A *= x;
+//        }
+//        else if (op == "/" && x != 0) {
+//            A /= x;
+//        }
+//        else {
+//            cout << "error" << endl;
+//            break;
+//        }
+//        cout << i + 1 << ":" << A << endl;
+//    }
+//}

c20091a4960d44c22388b08bf6e28295d5d4a306
0429oonishi <oonishireon110@gmail.com>
2021-05-30 13:31:05 +0900

    add: ex13

---
+++ CPP-practice/ex13.cpp
@@ -0,0 +1,29 @@
+//
+//  ex13.cpp
+//  CPP-practice
+//
+//  Created by 大西玲音 on 2021/05/29.
+//
+
+#include "include/bits/stdc++.h"
+using namespace std;
+
+//int main() {
+//    int N;
+//    int total = 0;
+//    cin >> N;
+//    vector<int> A(N);
+//    for (int i = 0; i < N; i++) {
+//        cin >> A.at(i);
+//        total += A.at(i);
+//    }
+//    int average = total / A.size();
+//    for (int i = 0; i < N; i++) {
+//        if (average < A.at(i)) {
+//            cout << A.at(i) - average << endl;
+//        }
+//        else {
+//            cout << average - A.at(i) << endl;
+//        }
+//    }
+//}

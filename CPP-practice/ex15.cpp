500a61a987cb92ab258199633225a637fdba1e4a
0429oonishi <oonishireon110@gmail.com>
2021-05-30 15:50:31 +0900

    add: ex15

---
+++ CPP-practice/ex15.cpp
@@ -0,0 +1,42 @@
+//
+//  ex15.cpp
+//  CPP-practice
+//
+//  Created by 大西玲音 on 2021/05/29.
+//
+
+#include "include/bits/stdc++.h"
+using namespace std;
+
+int sum(vector<int> scores) {
+    int total = 0;
+    for (int i = 0; i < scores.size(); i++) {
+        total += scores.at(i);
+    }
+    return total;
+}
+
+void output(int sum_a, int sum_b, int sum_c) {
+    int budget = sum_a * sum_b * sum_c;
+    cout << budget << endl;
+}
+
+vector<int> input(int N) {
+    vector<int> vec(N);
+    for (int i = 0; i < N; i++) {
+        cin >> vec.at(i);
+    }
+    return vec;
+}
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A = input(N);
+    vector<int> B = input(N);
+    vector<int> C = input(N);
+    int sum_A = sum(A);
+    int sum_B = sum(B);
+    int sum_C = sum(C);
+    output(sum_A, sum_B, sum_C);
+}

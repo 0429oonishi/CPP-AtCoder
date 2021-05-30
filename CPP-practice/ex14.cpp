08b0ebb6cb7cb5a3b1fc85a3ea22b38fb8527e7e
0429oonishi <oonishireon110@gmail.com>
2021-05-30 14:15:13 +0900

    add: ex14

---
+++ CPP-practice/ex14.cpp
@@ -0,0 +1,17 @@
+//
+//  ex14.cpp
+//  CPP-practice
+//
+//  Created by 大西玲音 on 2021/05/29.
+//
+
+#include "include/bits/stdc++.h"
+using namespace std;
+
+//int main() {
+//    int A, B, C;
+//    cin >> A >> B >> C;
+//    int heigh = max(max(A, B), C);
+//    int low = min(min(A, B), C);
+//    cout << heigh - low << endl;
+//}

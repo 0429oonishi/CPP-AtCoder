4f5f1a59adc85c8c4dfec8be352177297f4f5187
0429oonishi <oonishireon110@gmail.com>
2021-06-02 10:04:29 +0900

    add: ex16

---
+++ CPP-practice/ex16.cpp
@@ -0,0 +1,28 @@
+//
+//  ex16.cpp
+//  CPP-practice
+//
+//  Created by 大西玲音 on 2021/05/30.
+//
+
+#include "include/bits/stdc++.h"
+using namespace std;
+
+//int main() {
+//    vector<int> data(5);
+//    for (int i = 0; i < 5; i++) {
+//        cin >> data.at(i);
+//    }
+//    bool isSame = false;
+//    for (int i = 0; i < 4; i++) {
+//        if (data.at(i) == data.at(i+1)) {
+//            isSame = true;
+//        }
+//    }
+//    if (isSame) {
+//        cout << "YES" << endl;
+//    }
+//    else {
+//        cout << "NO" << endl;
+//    }
+//}

9d09bf6907fa1d570890a9470b0d9b863377cf52
0429oonishi <oonishireon110@gmail.com>
2021-06-02 10:19:38 +0900

    add: ex17

---
+++ CPP-practice/ex17.cpp
@@ -0,0 +1,38 @@
+//
+//  ex17.cpp
+//  CPP-practice
+//
+//  Created by 大西玲音 on 2021/05/30.
+//
+
+#include "include/bits/stdc++.h"
+using namespace std;
+
+//int main() {
+//    int N, S;
+//    cin >> N >> S;
+//    vector<int> A(N), P(N);
+//    for (int i = 0; i < N; i++) {
+//        cin >> A.at(i);
+//    }
+//    for (int i = 0; i < N; i++) {
+//        cin >> P.at(i);
+//    }
+//    int count = 0;
+//    for (int i = 0; i < N; i++) {
+//        for (int j = 0; j < N; j++) {
+//            if (A.at(i) + P.at(j) == S) {
+//                count++;
+//            }
+//        }
+//    }
+////    // 範囲for文を使った別解
+////    for (int x: A) {
+////        for (int y: P) {
+////            if (x + y == S) {
+////                cout++;
+////            }
+////        }
+////    }
+//    cout << count << endl;
+//}
